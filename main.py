b55022bd38d58008dd6e9dc6a01824b5f73f40d1
Kamil Głuszek <qacz707@gmail.com>
2022-05-16 02:23:08 +0200

    Initial commit

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class GiveMeSomethingResp(BaseModel):
 def receive_something(rq: GiveMeSomethingRq):
     return GiveMeSomethingResp(received=rq.dict())
 
-@app.get("/day/{name}{number}", status_code=200)
+@app.get("/day/{name}&{number}", status_code=200)
 def method_get(name: str, number: str, response: Response):
     weekday = {"monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6, "sunday": 7}
     if weekday[name.lower()] != number:
