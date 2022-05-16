6f22a078732b73c02c5d06d0eaf17cbce01607c6
Kamil Głuszek <qacz707@gmail.com>
2022-05-16 03:03:22 +0200

    Initial commit

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class HelloResp2(BaseModel):
 @app.get("/day{name}&{number}", status_code=200)
 def method_get(name: str, number: int, response: Response):
     weekday = {"monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6, "sunday": 7}
-    # if weekday[name.lower()] != number:
-    #     response.status_code = status.HTTP_400_BAD_REQUEST
-    #     return "Błąd"
+    if weekday[name.lower()] != number:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return "Błąd"
     return number
